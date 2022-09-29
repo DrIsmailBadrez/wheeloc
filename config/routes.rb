30f38acf5ecde827f441f608e73b1fec38a1f219
amine karri <attialod@gmail.com>
2022-09-29 03:34:57 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,13 +7,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/myoffers', to: 'pages#myoffers'
-  get '/mybookings', to: 'pages#mybookings'
   get '/aboutus', to: 'pages#aboutus'
   get '/contact', to: 'pages#contact'
 
   resources :users, only: %i[edit update show]
   resources :offers do
-    resources :bookings, only: %i[new create]
+
   end
-  resources :bookings, except: %i[new create]
+
 end
